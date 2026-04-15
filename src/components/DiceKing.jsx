db130551438adaf2b74143c1b2ee7e8353c9a4d9
Kelly <kelly.gama.g@gmail.com>
2026-04-15 11:42:18 +0200

    feat: diceHands & diceIdle

--- src/components/DiceKing.jsx
+++ src/components/DiceKing.jsx
@@ -1,16 +1,48 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useSpriteLoop } from "../hooks/useSpriteLoop";
 
-const diceFrames = Array.from({ length: 48 }, (_, i) =>
+
+const diceHandsFrames = Array.from({ length: 17 }, (_, i) =>
+  `/assets/DiceKing/introHands/boss-battle-kingdice-intro_${String(i + 1).padStart(4, "0")}.png`
+);
+
+const diceIntroFrames = Array.from({ length: 48 }, (_, i) =>
   `/assets/DiceKing/intro/boss-battle-kingdice-intro_${String(i + 1).padStart(4, "0")}.png`
+);
+
+const diceIdleFrames = Array.from({ length: 33 }, (_, i) =>
+  `/assets/DiceKing/idle/boss-battle-kingdice-idle_${String(i + 1).padStart(4, "0")}.png`
 );
 
 export default function DiceKingIntro() {
   const diceRef = useRef(null);
 
-  useSpriteLoop(diceFrames, 35, diceRef);
+  const [phase, setPhase] = useState("hands");
+  
+  const currentFrames = 
+    phase === "hands" ? 
+    diceHandsFrames 
+    : phase === "intro"
+    ? diceIntroFrames
+    : diceIdleFrames;
+
+  useSpriteLoop(
+    currentFrames,
+    35,
+    diceRef,
+    phase === "idle",
+    () => {
+        if (phase === "hands") {
+            setPhase("intro");
+        } else if (phase === "intro") {
+            setPhase("idle");
+        }
+    }
+    );
 
   return (
-      <img ref={diceRef} className="dice" src={diceFrames[0]} />
+      <img ref={diceRef} 
+      className={`dice ${phase === "hands" ? "hands" : ""}`}
+      src={currentFrames[0]} />
   );
 }
