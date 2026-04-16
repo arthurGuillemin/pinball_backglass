87b1da7cd6a667f7c835cfeae93c939d5718be59
Kelly <kelly.gama.g@gmail.com>
2026-04-16 10:58:54 +0200

    feat: introScreen

---
+++ src/pages/IntroScreen.jsx
@@ -0,0 +1,42 @@
+import { useRef } from "react";
+import { useSpriteLoop } from "../hooks/useSpriteLoop";
+
+const animationFrames = Array.from({ length: 34 }, (_, i) =>
+  `/assets/IntroScreen/Cuphead & Mugman/cuphead_title_screen_${String(i + 1).padStart(4, "0")}.png`
+);
+
+export default function IntroScreen() {
+  const animRef = useRef(null);
+
+  useSpriteLoop( animationFrames, 20, animRef, true );
+
+  return (
+    <div className="intro-container">
+
+      <img
+        src="/assets/IntroScreen/title_screen_background.png"
+        alt="background"
+        className="intro-bg"
+      />
+
+      <img
+        src="/assets/IntroScreen/title_chips_left_0001.png"
+        alt="chips"
+        className="intro-chips"
+      />
+
+      <img
+        ref={animRef}
+        src={animationFrames[0]}
+        alt="cuphead animation"
+        className="intro-animation"
+      />
+
+
+      <div className="press-any-button">
+        PRESS ANY BUTTON
+       </div>
+
+    </div>
+  );
+}
