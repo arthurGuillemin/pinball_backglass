239a6c43c58cfb5f939572a5d15c1240bbc6e71f
Kelly <kelly.gama.g@gmail.com>
2026-05-19 12:44:43 +0200

    feat: RunNGun

--- src/pages/RunNGun.jsx
+++ src/pages/RunNGun.jsx
@@ -1,12 +1,15 @@
+
+import CupheadRun from "../components/CupheadRun";
+
 export default function VideoScreen() {
   return (
-    <div className="video-container">
-      <video autoPlay muted loop playsInline className="video-bg">
-        <source
-          src="/assets/IntroScreen/cuphead_forest_follies.mp4"
-          type="video/mp4"
-        />
-      </video>
+    <div className="scene">
+      <div className="sky" />
+      <div className="vista" />
+      <div className="mid" />
+
+      <CupheadRun phase="idle"/>
+
     </div>
   );
 }
